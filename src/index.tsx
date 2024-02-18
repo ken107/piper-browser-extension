c03391e64bf5df53128f867bf34841f7b3d47ede
Hai Phan <hai.phan@gmail.com>
2024-02-18 12:02:25 -0800

    good

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
+import { useImmer } from "use-immer"
 import config from "./config"
 import { getFile } from "./storage"
 import { MyVoice, PiperVoice } from "./types"
@@ -8,102 +9,122 @@ ReactDOM.createRoot(document.getElementById("app")!).render(<App />)
 
 
 function App() {
-  const [voiceList, setVoiceList] = React.useState<MyVoice[]>([])
+  const [state, stateUpdater] = useImmer({
+    voiceList: [] as MyVoice[],
+    activityLog: "Ready",
+  })
+  const refs = {
+    activityLog: React.useRef<HTMLTextAreaElement>(null!)
+  }
+  const installed = state.voiceList.filter(x => x.isInstalled)
+  const notInstalled = state.voiceList.filter(x => !x.isInstalled)
+
 
   //startup
   React.useEffect(() => {
     getVoiceList()
-      .then(setVoiceList)
+      .then(voiceList => stateUpdater(draft => {
+        draft.voiceList = voiceList
+      }))
       .catch(handleError)
-  }, [])
+  }, [
+  ])
 
-  //advertise
+  //advertise voices
   React.useEffect(() => {
-    (chrome.ttsEngine as any).updateVoices(
-      voiceList
-        .map(voice => ({
-          voiceName: voice.name,
-          lang: voice.languageCode,
-          eventTypes: ["start", "end", "error"]
-        }))
-        .sort((a, b) => a.lang.localeCompare(b.lang) || a.voiceName.localeCompare(b.voiceName))
-    )
-  }, [voiceList])
+    advertiseVoices(installed.length ? installed : notInstalled)
+  }, [
+    state.voiceList
+  ])
 
+  //scroll activity log
+  React.useEffect(() => {
+    refs.activityLog.current.scrollTop = refs.activityLog.current.scrollHeight
+  }, [
+    state.activityLog
+  ])
 
-  const installed = voiceList.filter(x => x.isInstalled)
-  const notInstalled = voiceList.filter(x => !x.isInstalled)
 
   return (
     <div className="container">
       <h2 className="text-muted">Activity Log</h2>
-      <textarea className="form-control" disabled rows={5}></textarea>
+      <textarea className="form-control" disabled rows={4} ref={refs.activityLog} value={state.activityLog} />
 
+      <h2 className="text-muted">Installed Voices ({installed.length})</h2>
+      {installed.length == 0 &&
+        <div className="text-muted">Installed voices will appear here</div>
+      }
       {installed.length > 0 &&
-        <React.Fragment>
-          <h4 className="text-muted">Installed Voices</h4>
-          <table className="table table-borderless table-hover table-sm">
-            <thead>
-              <tr>
-                <th>Voice</th>
-                <th>Language</th>
-                <th></th>
-                <th style={{width: "0%"}}></th>
+        <table className="table table-borderless table-hover table-sm">
+          <thead>
+            <tr>
+              <th>Voice</th>
+              <th>Language</th>
+              <th></th>
+              <th style={{width: "0%"}}></th>
+            </tr>
+          </thead>
+          <tbody>
+            {installed.map(voice =>
+              <tr key={voice.key}>
+                <td>
+                  <span className="me-1">{voice.name}</span>
+                  <span className="me-1">[{voice.quality}]</span>
+                  <span className="link" onClick={() => sampler.play(voice)}>sample</span>
+                </td>
+                <td>{voice.languageName}</td>
+                <td className="text-end pe-2">{(voice.modelFileSize /1e6).toFixed(1)}MB</td>
+                <td>
+                  <button type="button" className="btn btn-danger btn-sm">Delete</button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {installed.map(voice =>
-                <tr key={voice.key}>
-                  <td>
-                    <span className="me-1">{voice.name}</span>
-                    <span className="me-1">[{voice.quality}]</span>
-                    <span className="link" onClick={() => sampler.play(voice)}>sample</span>
-                  </td>
-                  <td>{voice.languageName}</td>
-                  <td className="text-end pe-2">{(voice.modelFileSize /1e6).toFixed(1)}MB</td>
-                  <td>
-                    <button type="button" className="btn btn-danger btn-sm">Delete</button>
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </React.Fragment>
+            )}
+          </tbody>
+        </table>
       }
 
+      <h2 className="text-muted">Available to Install ({notInstalled.length})</h2>
       {notInstalled.length > 0 &&
-        <React.Fragment>
-          <h2 className="text-muted">Available to Install</h2>
-          <table className="table table-borderless table-hover table-sm">
-            <thead>
-              <tr>
-                <th>Voice</th>
-                <th>Language</th>
-                <th></th>
-                <th style={{width: "0%"}}></th>
+        <table className="table table-borderless table-hover table-sm">
+          <thead>
+            <tr>
+              <th>Voice</th>
+              <th>Language</th>
+              <th></th>
+              <th style={{width: "0%"}}></th>
+            </tr>
+          </thead>
+          <tbody>
+            {notInstalled.map(voice =>
+              <tr key={voice.key}>
+                <td>
+                  <span className="me-1">{voice.name}</span>
+                  <span className="me-1">[{voice.quality}]</span>
+                  <span className="link" onClick={() => sampler.play(voice)}>sample</span>
+                </td>
+                <td>{voice.languageName}</td>
+                <td className="text-end pe-2">{(voice.modelFileSize /1e6).toFixed(1)}MB</td>
+                <td>
+                  <button type="button" className="btn btn-success btn-sm">Install</button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {notInstalled.map(voice =>
-                <tr key={voice.key}>
-                  <td>
-                    <span className="me-1">{voice.name}</span>
-                    <span className="me-1">[{voice.quality}]</span>
-                    <span className="link" onClick={() => sampler.play(voice)}>sample</span>
-                  </td>
-                  <td>{voice.languageName}</td>
-                  <td className="text-end pe-2">{(voice.modelFileSize /1e6).toFixed(1)}MB</td>
-                  <td>
-                    <button type="button" className="btn btn-success btn-sm">Install</button>
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </React.Fragment>
+            )}
+          </tbody>
+        </table>
       }
     </div>
   )
+
+  function handleError(err: unknown) {
+    console.error(err)
+    appendActivityLog(String(err))
+  }
+
+  function appendActivityLog(text: string) {
+    stateUpdater(draft => {
+      draft.activityLog += "\n" + text
+    })
+  }
 }
 
 
@@ -132,14 +153,22 @@ async function getVoiceList(): Promise<MyVoice[]> {
   return voiceList
 }
 
+function advertiseVoices(voices: MyVoice[]) {
+  (chrome.ttsEngine as any).updateVoices(
+    voices
+      .map(voice => ({
+        voiceName: voice.name,
+        lang: voice.languageCode,
+        eventTypes: ["start", "end", "error"]
+      }))
+      .sort((a, b) => a.lang.localeCompare(b.lang) || a.voiceName.localeCompare(b.voiceName))
+  )
+}
+
 async function piperFetch(file: string): Promise<Blob> {
   const res = await fetch(config.repoUrl + file)
   if (!res.ok) throw new Error("Server return " + res.status)
   return res.blob()
-}
-
-function handleError(err: unknown) {
-  console.error(err)
 }
 
 function immediate<T>(func: () => T) {
